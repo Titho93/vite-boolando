9ad087bc833be7448e3fde4fc1a2d912b9bea2e1
titho93 <titho93.10@gmail.com>
2023-10-10 15:46:20 +0200

    [edit]Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,28 +8,29 @@ export default {
 
     <header>
         <div class="sticky">   
-      <div class="container">
+            <div class="container">
       
-        <div class="bar">
-          <div class="menu">
-            <ul>
-              <li><a href="#"><strong>Donna</strong></a></li>
-              <li><a href="#"><strong>Uomo</strong></a></li>
-              <li><a href="#"><strong>Bambini</strong></a></li>
-                          
-            </ul>
-          </div>
-          <div class="logo"></div>
-          <div class="action">
-            <ul>
-              <li> <i class="fa-regular fa-user"></i></li>
-              <li><i class="fa-regular fa-heart"></i></li>
-              <li><i class="fa-solid fa-bag-shopping"></i></li>
-            </ul>
-          </div>
+                <div class="bar">
+                    <div class="menu">
+                        <ul>
+                            <li><a href="#"><strong>Donna</strong></a></li>
+                            <li><a href="#"><strong>Uomo</strong></a></li>
+                            <li><a href="#"><strong>Bambini</strong></a></li>
+                                        
+                        </ul>
+                    </div>
+               
+                    <div class="logo"></div>
+                    <div class="action">
+                        <ul>
+                            <li> <i class="fa-regular fa-user"></i></li>
+                            <li><i class="fa-regular fa-heart"></i></li>
+                            <li><i class="fa-solid fa-bag-shopping"></i></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
     </header>
   
 </template>
@@ -46,8 +47,43 @@ header {
         top: 0;
         height: 70px;
         z-index: 5;
-        background-color: #EF6630;
+        background-color: $primary-color;
         width: 100%
+    }    
+    .container {
+        margin: 0 auto;
+        width: 90%;
+        max-width: 1200px;
+        padding: 20px 0;
+    }
+    .bar {
+        display: flex;
+        justify-content: space-between;
+        height: 40px;
+    }
+    .menu {
+        cursor: pointer;
+        left: 0;
+        color: $text-primary-color;
+    }
+    ul {
+    display: flex;
+    list-style-type: none;
+    }
+    .action {
+        cursor: pointer;
+        right: 0;
+        color: $text-primary-color;
+    }
+    .logo {
+        text-align: center;
+        background-image: url(/boolean-logo.png);
+        width: 130px;
+        height: 30px;
+        background-position: center;
+        background-size: contain;
+        background-repeat: no-repeat;
+        margin-left: -130px;
     }
 }
 
