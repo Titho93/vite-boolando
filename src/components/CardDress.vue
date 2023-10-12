15fa0fa2bb4027b67b4121877b8ba2dba1fe3b02
titho93 <titho93.10@gmail.com>
2023-10-12 09:46:27 +0200

    [fix]discount bug

--- src/components/CardDress.vue
+++ src/components/CardDress.vue
@@ -17,11 +17,11 @@ export default {
 
     <div class="card">
         <div class="img">
-            <img class="model" :src="`/assets/${cardObj.primaryImage}`" alt="1.webp">
-            <img class="model-2" :src="`/assets/${cardObj.secondaryImage}`" alt="1b.webp">
+            <img class="model" :src="`/assets/${cardObj.primaryImage}`" alt="cardObj.modello">
+            <img class="model-2" :src="`/assets/${cardObj.secondaryImage}`" alt="name object..">
             <span class="heart"><i class="fa-solid fa-heart"></i></span>
-            <span class="discount">{{ cardObj.discount }}</span>
-            <span class="eco">{{ cardObj.sostenibilita}}</span>
+            <span v-if="cardObj.discount" class="discount">{{ cardObj.discount }}</span>
+            <span v-if="cardObj.sostenibilita" class="eco">{{ cardObj.sostenibilita}}</span>
         </div>
         <div class="bio">
             <h4>{{ cardObj.marca  }}</h4>
